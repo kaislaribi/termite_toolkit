f7a640c03fcd905e8f2eb51e4fa076895ec9b719
Joseph Mullen <JMMull@users.noreply.github.com>
2019-01-22 09:39:26 +0000

    Updated the call without toolkit script

--- sample_scripts/call_without_toolkit.py
+++ sample_scripts/call_without_toolkit.py
@@ -7,17 +7,22 @@ Example script for multipart call using requests.
 import requests
 import os
 
-testfile = "medline_sample.zip"
+__author__ = 'Joe Mullen'
+__version__ = '1.0'
+
+MEDLINEZIP = "medline_sample.zip"
+TERMITE_URL = 'http://localhost:9090/termite'
+CERTIFICATE_PATH = 'PATH_TO_CERT' # or False
 
 
-def get_termite_xml(zipfile, format='medline.xml', output='xml'):
+def call_termite_file(zipfile, format='medline.xml', output='json'):
     """
     Function for sending a multipart request to TERMite.
 
-    :param zipfile:
-    :param format:
-    :param output:
-    :return:
+    :param zipfile: path to zip file
+    :param format: input format
+    :param output: output format
+    :return: response text
     """
 
     url = 'http://localhost:9090/termite'
@@ -25,17 +30,43 @@ def get_termite_xml(zipfile, format='medline.xml', output='xml'):
         'format': format,
         'output': output,
         'subsume': 'true',
-        'rejectMinorHits' : 'true',
-        'fuzzy' : 'true',
-        'opts' : 'fzy.promote=true'
+        'rejectMinorHits': 'true',
+        'fuzzy': 'true',
+        'opts': 'fragmentSize=20&fzy.promote=true&rejectAmbig=false'
     }
 
     file_obj = open(zipfile, 'rb')
     file_name = os.path.basename(zipfile)
     binary_content = {"binary": (file_name, file_obj)}
-    response = requests.post(url, data=form_data, files=binary_content)
+    response = requests.post(url, data=form_data, files=binary_content, verify=CERTIFICATE_PATH)
 
     return response.text
 
 
-print(get_termite_xml(testfile))
+def call_termite_txt(text, format='txt', output='json'):
+    """
+    Function for sending a text request to TERMite.
+
+    :param text: string to be annotated
+    :param format: input format
+    :param output: output format
+    :return: response text
+    """
+
+    form_data = {
+        'text': text,
+        'format': format,
+        'output': output,
+        'subsume': 'true',
+        'rejectMinorHits': 'true',
+        'fuzzy': 'true',
+        'opts': 'fragmentSize=20&fzy.promote=true&rejectAmbig=false'
+    }
+
+    response = requests.post(TERMITE_URL, data=form_data, verify=CERTIFICATE_PATH)
+
+    return response.text
+
+
+print(call_termite_txt('citrate macrophage colony sildenafil stimulating factor influenza hedgehog'))
+print(call_termite_file(MEDLINEZIP))
